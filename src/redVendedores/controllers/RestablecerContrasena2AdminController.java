32e6af358ce52635f8092cf0a91ffce6cd6f8cfe
DanielaLosada <danielai.losadar@uqvirtual.edu.co>
2023-10-10 00:39:12 -0500

    Subir Interface nuevo cliente

--- src/redVendedores/controllers/RestablecerContrasena2AdminController.java
+++ src/redVendedores/controllers/RestablecerContrasena2AdminController.java
@@ -69,7 +69,7 @@ public class RestablecerContrasena2AdminController {
 	@FXML
  void initialize() {
 		modelFactoryController = ModelFactoryController.getInstance();
-		this.txtCodigoVerificacion=cadenaAleatoria();
+		//this.txtCodigoVerificacion=cadenaAleatoria();
  }
 
 	public void setAplicacion(Aplicacion aplicacion, String documento) {
@@ -77,21 +77,21 @@ public class RestablecerContrasena2AdminController {
 		this.documento= documento;
 	}
 
-	private Label cadenaAleatoria() {
-     String banco = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-     String cadenaAux= "";
-     for (int x = 0; x < 8; x++) {
-         int indiceAleatorio = numeroAleatorioEnRango(0, banco.length() - 1);
-         char caracterAleatorio = banco.charAt(indiceAleatorio);
-         cadenaAux+=caracterAleatorio;
-         txtCodigoVerificacion.setText(cadenaAux);
-     }
-     return txtCodigoVerificacion;
-
- }
-	private int numeroAleatorioEnRango(int minimo, int maximo) {
-     return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
- }
+//	private Label cadenaAleatoria() {
+//     String banco = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//     String cadenaAux= "";
+//     for (int x = 0; x < 8; x++) {
+//         int indiceAleatorio = numeroAleatorioEnRango(0, banco.length() - 1);
+//         char caracterAleatorio = banco.charAt(indiceAleatorio);
+//         cadenaAux+=caracterAleatorio;
+//         txtCodigoVerificacion.setText(cadenaAux);
+//     }
+//     return txtCodigoVerificacion;
+//
+// }
+//	private int numeroAleatorioEnRango(int minimo, int maximo) {
+//     return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
+// }
 
 	private void verificarCodigo(){
 		String codigoGenerado = txtCodigoVerificacion.getText();
